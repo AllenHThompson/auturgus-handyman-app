94d4af6e23d823bf56457f93bc0fd1749677ea9a
Allen H. Thompson <allenhthompson1@gmail.com>
2016-08-09 09:41:45 -0400

    Fixed app.run bug. Landing page is home.html.

--- front-end/app.js
+++ front-end/app.js
@@ -11,17 +11,25 @@ app.config(function($routeProvider){
      .when('/login', {
           templateUrl: 'login.html',
           controller: 'loginController'
-     });
+     })
+
+     .when('/services', {
+          templateUrl: 'services.html',
+          controller: 'servicesController'
+     })
 
 });
 
 app.controller('mainController', function(){
+     console.log("main.controller")
 
 });
 
-var API = 'some url'
+var API = 'some url';
 
-app.controller('loginController', function($scope, $http, $location, $cookies){
+app.controller('loginController', function($scope, $http, $location, $cookies) {
+     console.log("login.controller")
+
      var credentials = {
           "_id": null,
           "password": null,
@@ -29,20 +37,35 @@ app.controller('loginController', function($scope, $http, $location, $cookies){
      };
 
      $scope.login = function() {
+          // console.log("login controller")
           credentials._id = $scope.username;
           credentials.password = $scope.password;
           $http.post(API + '/login', credentials).success(function(data) {
                $cookies.put('Token', data.token);
-               $location.path('/options');
+               $location.path('/services');
           });
      };
 });
 
+app.controller('servicesController', function($scope, $http, $location) {
+     $http.get(API + '/services').success(function(data) {
+          $scope.services = data;
+     });
+
+     $scope.someService = function(someData) {
+          someData =  $scope
+     }
+})
+
 app.run(function($rootScope, $location, $cookies) {
      $rootScope.$on('$locationChangeStart', function(event, nextUrl, currentUrl) {
+
           currentUrl = currentUrl.split('#');
           nextUrl = nextUrl.split('#');
           token = $cookies.get('Token');
+
+          console.log("token:" + token, "currentUrl:" + currentUrl, "nextUrl:" + nextUrl)
+
           if (token === undefined) {
                if (nextUrl[1] === '/') {
                     $location.path('/');
@@ -50,9 +73,16 @@ app.run(function($rootScope, $location, $cookies) {
                     $location.path('/login');
                } else if (nextUrl[1] === '/register') {
                     $location.path('/register');
-               } else if (nextUrl[1] === '/options' || '/payment' || '/delivery') {
+               } else if (
+                    nextUrl[1] === '/services' ||
+                    nextUrl[1] === '/payment' ||
+                    nextUrl[1] === '/delivery') {
                     $location.path('/login');
                }
           }
+          if (token !== undefined) {
+               $location.path(nextUrl[1]);
+          }
+          $cookies.put('nextUrl', nextUrl[1]);
      });
 });
